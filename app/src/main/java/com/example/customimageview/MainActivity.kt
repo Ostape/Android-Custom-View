1be82fceda544d9779b5e5b0b82e7e7a21ce61e9
Orest Shemeliuk <orest_shemeliuk@epam.com>
2020-03-11 01:28:56 +0200

    change photo and create animation

--- app/src/main/java/com/example/customimageview/MainActivity.kt
+++ app/src/main/java/com/example/customimageview/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.customimageview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AnimationUtils.loadAnimation(this, R.anim.custom_anim).also { hyperspaceJumpAnimation ->
+            findViewById<ImageView>(R.id.imageViewTest1).startAnimation(hyperspaceJumpAnimation)
+        }
     }
 }
